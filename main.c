e1254b138302c140f9d06a97a261eb3f9232409e
Cameron Bright <cam13702605046@gmail.com>
2023-10-09 22:12:33 +0800

    此版本为修改前初始版本 充其量只能点个灯~

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/*
+program versions : 1.0
+
+此版本为修改前初始版本 充其量只能点个灯
+
+modification: 2023/10/9 22:09
+
+modifier: Cameron Bright
+
+*/
+
 #include <STC12C5A60S2.H>
 #include "LCD1602.h"	//包含LCD1602头文件
 void main()
